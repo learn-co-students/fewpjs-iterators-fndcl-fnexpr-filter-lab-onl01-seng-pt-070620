20734a48e88ae7316374e1ed9e0f46ba2f4a062f
Cristian Deleon <libaration@gmail.com>
2021-01-10 17:03:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const findMatching = (namesList, nameArg) => {
+   return namesList.filter(name => {
+        return name.toLowerCase() === nameArg.toLowerCase()
+    })
+}
+
+const fuzzyMatch = (namesList, nameArg) => {
+    return namesList.filter(name => {
+        return name.startsWith(nameArg)
+    })
+}
+
+const matchName = (namesObjs, nameArg) => {
+    return namesObjs.filter(obj => {
+        return obj.name === nameArg
+    })
+}
