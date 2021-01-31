327291c3a84b1bd374098f001befa550b6580375
Sean <Sransonette@gmail.com>
2021-01-31 13:56:48 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(list, driver) {
+    return list.filter( drivers => 
+        drivers.toLowerCase() === driver.toLowerCase()
+        )
+}
+
+function fuzzyMatch(list, driver) {
+    return list.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(driver.toLowerCase()) === 0
+    )
+}
+
+function matchName(source, soughtName) {
+    return source.filter( record => record.name === soughtName)
+  }
