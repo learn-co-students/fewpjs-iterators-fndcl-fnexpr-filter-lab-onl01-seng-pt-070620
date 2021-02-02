4e012eb94496bb39198f4d8bd4673e93378384d4
jmd <jordan.michael.dickey@gmail.com>
2021-02-01 20:41:08 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+function findMatching(names, name){
+    let matchingNames = names.filter(n => {
+        return n.toUpperCase() === name.toUpperCase();
+    })
+    return matchingNames
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter( driver =>
+        driver.toLowerCase().indexOf(letters.toLowerCase()) === 0
+      )
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => 
+        driver.name === name 
+    )
+}
+
